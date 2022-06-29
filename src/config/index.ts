5a9e9192304d9ad5d73a75269bea086edf08baa2
Stas Lysak <staslyzak@gmail.com>
2022-06-29 18:45:02 +0300

    fix

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,8 +1,10 @@
+import keys from './keys.json'
+
 export default {
   PORT: process.env.PORT,
   VERSION: process.env.npm_package_version,
   TG_BOT_TOKEN: '5522976913:AAFZRS78VJTzm9JMrUUiZ3wf6griRcy0xjw',
   // '5306221827:AAH_s-xSM5Y9krA7w7Uvj6Z5Fs0NaHpM-oA',
-  GOOGLE_SHEET_KEYS: require('./keys.json'),
+  GOOGLE_SHEET_KEYS: keys,
   GOOGLE_SHEET_ID: '1hn0OjovhxX3hkcL8a05AX_i_Ee9mSWTWhZZzrTjn3j8',
 }
